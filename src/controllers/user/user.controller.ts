32bd9a2fd203803ccb0572b36c3dbbf0600b6080
Aman Sharma <aman703jk@gmail.com>
2022-10-26 06:28:16 +0530

    Created APIs for create, verify user

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -4,7 +4,6 @@ import { UserManager } from './user.manager';
 import catchAsync from "../../utils/asyncWrapper";
 import { UserValidation } from './user.validation';
 
-
 export class UserController {
     public router = Router();
     private _userManager = new UserManager();
@@ -19,10 +18,33 @@ export class UserController {
             catchAsync(this._userValidation.createUser),
             catchAsync(this.createUser.bind(this))
         );
+        this.router.post('/verify',
+            catchAsync(this.verifyUser.bind(this))
+        );
+        this.router.post('/create-signature',
+            catchAsync(this.createSignature.bind(this))
+        );
     }
 
     private createUser = async (request: Request, response: Response) => {
-        const user = await this._userManager.createUser(request.body);
-        response.status(httpStatus.CREATED).send(user);
+        const { publicKey, privateKey } = await this._userManager.createSigningKey();
+        const createdUser = await this._userManager.createUser(request.body, publicKey);
+        response.status(httpStatus.CREATED).send({ createdUser, privateKey });
+    }
+
+    private createSignature = async (request: Request, response: Response) => {
+        const { message, privateKey } = request.body;
+        const signature = await this._userManager.createSignature(message, privateKey);
+        response.status(httpStatus.CREATED).send({ signature: signature });
+    }
+
+    private verifyUser = async (request: Request, response: Response) => {
+        const signature = request.header('x-auth-token');
+        if (!signature) {
+            return response.status(httpStatus.NOT_FOUND).json({ msg: 'Signature not found' });
+        }
+        const { userId, message } = request.body
+        const verifyUser = await this._userManager.verifyUser(userId, message, signature);
+        response.status(httpStatus.CREATED).send({ verifyUser: verifyUser });
     }
 }
--- src/controllers/user/user.manager.ts
+++ src/controllers/user/user.manager.ts
@@ -1,12 +1,58 @@
 import { ApiError } from "../../utils/ApiError";
 import { User } from "../../models/user.model";
 import httpStatus from "http-status";
+import _sodium from 'libsodium-wrappers';
+import { ObjectId } from "mongoose";
 
 export class UserManager {
-    public createUser = async (user: Partial<User>): Promise<User> => {
+    public createUser = async (user: Partial<User>, publicKey: string): Promise<User> => {
         if (user.email && await User.isEmailTaken(user.email)) {
             throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
         }
+        Object.assign(user, { publicKey: publicKey });
         return User.create(user);
+    }
+
+    public getUserById = async (id: ObjectId | string) => {
+        return User.findById(id);
+    };
+
+    public convertStrToAB = async (str: string) => {
+        return new Uint8Array(Buffer.from(str, 'base64'));
+    }
+
+    public createSigningKey = async () => {
+        await _sodium.ready;
+        const sodium = _sodium;
+
+        const keyspair = sodium.crypto_sign_keypair();
+
+        const publicKey = Buffer.from(keyspair.publicKey).toString('base64');
+        const privateKey = Buffer.from(keyspair.privateKey).toString('base64');
+
+        return { publicKey, privateKey };
+    }
+
+    public createSignature = async (message: string, privateKey: any) => {
+        await _sodium.ready;
+        const sodium = _sodium;
+        let uint8Array = await this.convertStrToAB(privateKey);
+        const signature = sodium.crypto_sign_detached(message, uint8Array)
+        return Buffer.from(signature).toString('base64');
+    }
+
+    public verifyUser = async (userId: ObjectId | string, message: string, signature: any) => {
+        const user = await this.getUserById(userId);
+        if (!user) {
+            throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+        }
+        await _sodium.ready;
+        const sodium = _sodium;
+        let signatureBuffer = await this.convertStrToAB(signature);
+        let publicKeyBuffer = await this.convertStrToAB(user.publicKey);
+
+        const isVerified = sodium.crypto_sign_verify_detached(signatureBuffer, message, publicKeyBuffer);
+
+        return isVerified;
     }
 }
